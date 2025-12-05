9dc7eca487973018095abb7b8e4b5af30c5836f6
farshadjanmohammdi@gmail.com <farshad.janmohammadi@gmail.com>
2025-12-05 11:19:45 +0330

    Revert "fix app"
    This reverts commit a49d4cd845e6be0b95278d62515c50455f6b5054.

--- src/app.js
+++ src/app.js
@@ -2,167 +2,259 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const path = require("path");
+const cluster = require("cluster");
 const http = require("http");
 const https = require("https");
-const fs = require("fs");
 const config = require("../config");
 const baseUrl = require("./utils").baseUrl;
+var fs = require("fs");
+const PORT = config.web.port;
+const PORT_S = config.web.portS;
 const {
   isEmptyObject,
   isArray,
   isObject,
   safelyParseJson,
 } = require("../helpers/helper");
+const { body } = require("express-validator");
+const { use } = require("../routes/config");
 const { updateAPICaches } = require("../controllers/update.controller");
 
-const PORT = config.web.port;
-const PORT_S = config.web.portS;
+const setupServer = () => {
+  if (cluster.isMaster) {
+    let numWorkers = require("os").cpus().length;
 
-const app = express();
+    console.log("Master cluster setting up " + numWorkers + " workers...");
 
-// ------------------- CORS -------------------
-if (process.env.NODE_ENV !== "production") {
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", `*`);
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    for (let i = 0; i < numWorkers; i++) {
+      if (i === 0) {
+        // Run tasks
+        setTimeout(() => {
+          updateAPICaches();
+        }, 5000);
+
+        require("../tasks");
+      }
+      cluster.fork();
+    }
+
+    cluster.on("online", function (worker) {
+      console.log("Worker " + worker.process.pid + " is online");
+    });
+
+    cluster.on("exit", function (worker, code, signal) {
+      console.log(
+        "Worker " +
+          worker.process.pid +
+          " died with code: " +
+          code +
+          ", and signal: " +
+          signal
+      );
+      console.log("Starting a new worker");
+      cluster.fork();
+    });
+  } else {
+    const app = express();
+    if (process.env.NODE_ENV == "production") {
+      var https_options = {
+        key: fs.readFileSync("./crk/pay.digihyper.key"),
+        cert: fs.readFileSync("./crk/7dd9e521b99adfd6.pem"),
+        ca: [
+          fs.readFileSync("./crk/7dd9e521b99adfd6.crt"),
+          fs.readFileSync("./crk/gd_bundle-g2-g1.crt"),
+        ],
+      };
+    }
+
+    if (process.env.NODE_ENV != "production") {
+      app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", `*`);
+        res.header(
+          "Access-Control-Allow-Methods",
+          "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+        );
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Content-Type, Accept, Authorization, Password, Token"
+        );
+        res.header("Access-Control-Allow-Credentials", "true");
+        res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+        next();
+      });
+    }
+
+    if (process.env.NODE_ENV == "production") {
+      app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header(
+          "Access-Control-Allow-Methods",
+          "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+        );
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Content-Type, Accept, Authorization, Password, Token"
+        );
+        res.header("Access-Control-Allow-Credentials", "true");
+        res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+        next();
+      });
+    }
+
+    app.options("/*", (_, res) => {
+      res.sendStatus(200);
+    });
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    app.use(
+      bodyParser.json({
+        limit: "8000kb",
+      })
     );
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Accept, Authorization, Password, Token"
+    app.disable("x-powered-by");
+    morgan.token("api-content", (req, res) => {
+      const bodyLog = {
+        method: req.method,
+        url: req.url,
+        statusCode: res.statusCode,
+        responseTime: res.responseTime,
+      };
+      return JSON.stringify(bodyLog);
+    });
+    morgan.token("request-content", (req, res) => {
+      var userId = null;
+      if (req.user) {
+        userId = req.user._id;
+      }
+      var bodyLog = null;
+      if (req.body && !isEmptyObject(req.body)) {
+        bodyLog = { RequestBody: req.body };
+      } else if (req.params && !isEmptyObject(req.params)) {
+        bodyLog = { RequestParams: req.params };
+      } else if (req.query && !isEmptyObject(req.query)) {
+        bodyLog = { RequestQuery: req.query };
+      } else {
+        bodyLog = { RequestQuery: {} };
+      }
+      if (!userId) return "\n" + JSON.stringify({ ...bodyLog });
+      else return "\n" + JSON.stringify({ ...bodyLog, ...{ userId: userId } });
+    });
+
+    morgan.token("response-content", (req, res) => {
+      var userId = null;
+      var bodyLog = null;
+      if (req.user) {
+        userId = req.user._id;
+      }
+      var body = safelyParseJson(res._body ? res._body : "{}");
+      if (!body) {
+        return JSON.stringify({});
+      }
+      if (!body.status) {
+        bodyLog = { ResponseBody: body };
+      } else if (!body.response) {
+        bodyLog = { ResponseBody: body };
+      } else {
+        const keys = Object.keys(body.response);
+        for (const i in keys) {
+          if (isObject(body.response[keys[i]])) {
+            bodyLog = {
+              ResponseBody: { status: true, response: body.response },
+            };
+          } else if (isArray(body.response[keys[i]])) {
+            bodyLog = {
+              ResponseBody: {
+                status: true,
+                count: body.response[keys[i]].length,
+              },
+            };
+          } else {
+            bodyLog = { ResponseBody: { status: true } };
+          }
+        }
+      }
+      if (!userId) return JSON.stringify({ ...bodyLog });
+      else return JSON.stringify({ ...bodyLog, ...{ userId: userId } });
+    });
+
+    app.use(saveResponseBody);
+    app.use(saveResponseTime);
+    app.use(
+      morgan(
+        ":api-content :request-content \n:response-content \n--------------- "
+      )
     );
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
-    next();
-  });
-}
+    app.use("/public", express.static(path.join(__dirname, "../public")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Accept, Authorization, Password, Token"
-    );
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
-    next();
-  });
-}
+    // Routes to Alert
+    app.use(baseUrl.concat("/alert"), require("../routes/api/alert"));
+    // Routes to User
+    app.use(baseUrl.concat("/user"), require("../routes/api/user"));
+    // Routes to Basket
+    app.use(baseUrl.concat("/user"), require("../routes/api/basket"));
+    // Routes to plan
+    app.use(baseUrl.concat("/plan"), require("../routes/api/plan"));
+    // Routes to message
+    app.use(baseUrl.concat("/message"), require("../routes/api/message"));
+    // Routes to category
+    app.use(baseUrl.concat("/category"), require("../routes/api/category"));
+    // Routes to channel
+    app.use(baseUrl.concat("/channel"), require("../routes/api/channel"));
+    // Routes to service
+    app.use(baseUrl.concat("/service"), require("../routes/api/service"));
+    // Routes to Banner
+    app.use(baseUrl.concat("/banner"), require("../routes/api/banner"));
+    // Routes to client
+    app.use(baseUrl.concat("/"), require("../routes/api/client"));
+    // Config route
+    app.use(baseUrl.concat("/"), require("../routes/config"));
+    // Admin route
+    app.use(baseUrl.concat("/admin"), require("../routes/api/admin"));
+    // Payment
+    app.use(baseUrl.concat("/payment"), require("../routes/api/payment"));
+    // brokerage
+    app.use(baseUrl.concat("/brokerage"), require("../routes/api/brokerage"));
+    // exchanger
+    app.use(baseUrl.concat("/exchanger"), require("../routes/api/exchanger"));
+    // popup
+    app.use(baseUrl.concat("/popup"), require("../routes/api/popup"));
+    // rss
+    app.use(baseUrl.concat("/rss"), require("../routes/api/rss"));
 
-app.options("/*", (_, res) => res.sendStatus(200));
+    http.createServer(app).listen(PORT, () => {
+      console.log(
+        `Started server on => http://0.0.0.0:${PORT} for Process Id ${process.pid}`
+      );
+    });
+    if (process.env.NODE_ENV == "production") {
+      https.createServer(https_options, app).listen(PORT_S, () => {
+        console.log(
+          `Started server on => https://0.0.0.0:${PORT_S} for Process Id ${process.pid}`
+        );
+      });
+    }
 
-// ------------------- Body parser -------------------
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: "8000kb" }));
+    console.log(`Worker ${process.pid} started`);
+  }
+};
 
-// ------------------- Security -------------------
-app.disable("x-powered-by");
+module.exports = {
+  setupServer,
+};
 
-// ------------------- Logging -------------------
-morgan.token("api-content", (req, res) => {
-  return JSON.stringify({
-    method: req.method,
-    url: req.url,
-    statusCode: res.statusCode,
-    responseTime: res.responseTime,
-  });
-});
-
-morgan.token("request-content", (req, res) => {
-  let userId = req.user ? req.user._id : null;
-  let bodyLog = isEmptyObject(req.body) ? {} : { RequestBody: req.body };
-  return JSON.stringify(userId ? { ...bodyLog, userId } : bodyLog);
-});
-
-morgan.token("response-content", (req, res) => {
-  let userId = req.user ? req.user._id : null;
-  let body = safelyParseJson(res._body || "{}");
-  let bodyLog = body?.status
-    ? { ResponseBody: { status: true } }
-    : { ResponseBody: body };
-  return JSON.stringify(userId ? { ...bodyLog, userId } : bodyLog);
-});
-
-app.use(saveResponseBody);
-app.use(saveResponseTime);
-app.use(
-  morgan(":api-content :request-content \n:response-content \n--------------- ")
-);
-
-// ------------------- Static -------------------
-app.use("/public", express.static(path.join(__dirname, "../public")));
-
-// ------------------- Routes -------------------
-app.use(baseUrl.concat("/alert"), require("../routes/api/alert"));
-app.use(baseUrl.concat("/user"), require("../routes/api/user"));
-app.use(baseUrl.concat("/user"), require("../routes/api/basket"));
-app.use(baseUrl.concat("/plan"), require("../routes/api/plan"));
-app.use(baseUrl.concat("/message"), require("../routes/api/message"));
-app.use(baseUrl.concat("/category"), require("../routes/api/category"));
-app.use(baseUrl.concat("/channel"), require("../routes/api/channel"));
-app.use(baseUrl.concat("/service"), require("../routes/api/service"));
-app.use(baseUrl.concat("/banner"), require("../routes/api/banner"));
-app.use(baseUrl.concat("/"), require("../routes/api/client"));
-app.use(baseUrl.concat("/"), require("../routes/config"));
-app.use(baseUrl.concat("/admin"), require("../routes/api/admin"));
-app.use(baseUrl.concat("/payment"), require("../routes/api/payment"));
-app.use(baseUrl.concat("/brokerage"), require("../routes/api/brokerage"));
-app.use(baseUrl.concat("/exchanger"), require("../routes/api/exchanger"));
-app.use(baseUrl.concat("/popup"), require("../routes/api/popup"));
-app.use(baseUrl.concat("/rss"), require("../routes/api/rss"));
-
-// ------------------- Start server -------------------
-http.createServer(app).listen(PORT, () => {
-  console.log(
-    `Started server on => http://0.0.0.0:${PORT} for Process Id ${process.pid}`
-  );
-});
-
-if (process.env.NODE_ENV === "production") {
-  const https_options = {
-    key: fs.readFileSync("./crk/pay.digihyper.key"),
-    cert: fs.readFileSync("./crk/7dd9e521b99adfd6.pem"),
-    ca: [
-      fs.readFileSync("./crk/7dd9e521b99adfd6.crt"),
-      fs.readFileSync("./crk/gd_bundle-g2-g1.crt"),
-    ],
-  };
-
-  https.createServer(https_options, app).listen(PORT_S, () => {
-    console.log(
-      `Started server on => https://0.0.0.0:${PORT_S} for Process Id ${process.pid}`
-    );
-  });
-}
-
-// ------------------- Optional tasks -------------------
-setTimeout(() => {
-  updateAPICaches();
-}, 5000);
-
-require("../tasks");
-
-console.log(`Worker ${process.pid} started`);
-
-// ------------------- Helpers -------------------
 function saveResponseBody(req, res, next) {
-  let oldWrite = res.write;
-  let oldEnd = res.end;
-  let chunks = [];
+  var oldWrite = res.write;
+  var oldEnd = res.end;
+  var chunks = [];
   res.write = function (chunk) {
     chunks.push(chunk);
     return oldWrite.apply(res, arguments);
   };
   res.end = function (chunk) {
     if (chunk && Buffer.isBuffer(chunk)) chunks.push(chunk);
-    res._body = Buffer.concat(chunks).toString("utf8");
+    var body = Buffer.concat(chunks).toString("utf8");
+    res._body = body;
     oldEnd.apply(res, arguments);
   };
   next();
@@ -172,15 +264,15 @@ const getDurationInMilliseconds = (start) => {
   const NS_PER_SEC = 1e9;
   const NS_TO_MS = 1e6;
   const diff = process.hrtime(start);
+
   return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
 };
 
-function saveResponseTime(req, res, next) {
+const saveResponseTime = (req, res, next) => {
   const start = process.hrtime();
   res.on("finish", () => {
-    res.responseTime = getDurationInMilliseconds(start).toLocaleString();
+    const durationInMilliseconds = getDurationInMilliseconds(start);
+    res.responseTime = durationInMilliseconds.toLocaleString();
   });
   next();
-}
-
-module.exports = { app };
+};
