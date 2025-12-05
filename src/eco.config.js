e1d20501c2e7e93658b044fa5f720bc74831c070
farshadjanmohammdi@gmail.com <farshad.janmohammadi@gmail.com>
2025-12-05 15:10:06 +0330

    add redis

--- src/eco.config.js
+++ src/eco.config.js
@@ -12,7 +12,9 @@ module.exports = {
                 "BOURSE_RSS_CHANNEL_ID":"",
                 "CAR_AND_HOUSING_RSS_CHANNEL_ID":"",
                 "NEWS_CHANNEL_ID": "",
-                "T_START_ID": 1600
+                "T_START_ID": 1600,
+                "REDIS_HOST": "redis",
+                "REDIS_PORT": 6379
             },
             env_production: {
                 "NODE_ENV": "production",
@@ -24,7 +26,9 @@ module.exports = {
                 "MOSTVIEWS_RSS_CHANNEL_ID":"63a6a4edec165b4a23d6d3a8",
                 "CURRENCIES_RSS_CHANNEL_ID":"63a6a482ea86004a3043140c",
                 "NEWS_CHANNEL_ID": "60f7d000754eec33d2e3d0cd",
-                "T_START_ID": 100000
+                "T_START_ID": 100000,
+                "REDIS_HOST": "redis",
+                "REDIS_PORT": 6379
             },
             env_test: {
                 "NODE_ENV": "test",
@@ -37,7 +41,9 @@ module.exports = {
                 "CURRENCIES_RSS_CHANNEL_ID":"63a05f489a5fc207cbbbebba",
                 "NEWS_CHANNEL_ID": "612a2cb7446b096a3bc43801",
                 'JWT_SECRET_FORGET_PASS':'j!4P&mLKNXqxFC@w',
-                "T_START_ID": 1600
+                "T_START_ID": 1600,
+                "REDIS_HOST": "redis",
+                "REDIS_PORT": 6379
             }
         }
     ]
